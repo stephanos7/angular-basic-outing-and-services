0b041a7c1f649401c69e20697eca5e3456b4e68e
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-07-30 13:13:01 +0200

    add routing modules and initialise array of routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { JumboComponent } from './components/jumbo/jumbo.component';
+
+const routes : Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component:},
+  { path: '/about', component:}
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +18,8 @@ import { JumboComponent } from './components/jumbo/jumbo.component';
     JumboComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
