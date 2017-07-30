66d2c6b5123b47af1c8e6313053a3dacbb60699b
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-07-30 02:03:59 +0200

    add nav and jumbotron components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { JumboComponent } from './components/jumbo/jumbo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    JumboComponent
   ],
   imports: [
     BrowserModule
