8653b5d9f743553bb8a8afd3b8959bbe22074c3b
Stephanos Theodotou <stephanostheodotou@Stephanoss-MBP.home>
2017-07-30 15:14:02 +0200

    add placeholders

--- src/app/components/jumbo/jumbo.component.ts
+++ src/app/components/jumbo/jumbo.component.ts
@@ -3,9 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-jumbo',
   templateUrl: './jumbo.component.html',
-  styleUrls: ['./jumbo.component.css']
+  styleUrls: ['./jumbo.component.css'],
 })
+
 export class JumboComponent implements OnInit {
+h1Jumbo : string = "Welcome to Vint cinema";
+pJumbo : string = "lovingly built while practicing basic Angular 2 components, services and routing";
+forAbout: string;
 
   constructor() { }
 
